a8ff5b5efec7a2f55f9049a0b3f1d55fcb524c79
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-28 21:16:03 +0530

    Adding company schema. Closes #4

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,16 @@
 const graphql = require("graphql");
-const axios = require('axios')
+const axios = require("axios");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -9,6 +18,14 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: { 
+      type: CompanyType,
+      resolve(parentValue, args){
+        console.log(parentValue, args)
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+            .then(resp=>resp.data)
+      }
+    },
   },
 });
 
@@ -19,8 +36,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)
-            .then(resp => resp.data)
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
